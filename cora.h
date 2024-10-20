6deca15b37646c67cf6844db0b5e36b498d74397
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-10-20 16:55:04 +0300

    added somethings

---
+++ cora.h
@@ -0,0 +1,18 @@
+#ifndef CORA_H
+#define CORA_H
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <X11/Xlib.h>
+#endif
+
+typedef struct {
+
+} cora_window_t;
+
+typedef struct {
+
+} cora_event_t;
+
+#endif
--- cora_test.c
+++ cora_test.c
@@ -1,17 +1,17 @@
 #define CORA_IMPLEMENTATION
 #include "cora.h"
 
-#include <X11/Xlib.h>
-
 int main() {
-    XEvent event;
     Display* display = XOpenDisplay(NULL);
     Window w = XCreateSimpleWindow(display, DefaultRootWindow(display), 50, 50, 250, 250, 1, BlackPixel(display, 0), WhitePixel(display, 0));
     XMapWindow(display, w);
+
     XSelectInput(display, w, ExposureMask);
 
     for (;;) {
+        XEvent event;
         XNextEvent(display, &event);
+
         if (event.type == Expose) {
             XDrawString(display, w, DefaultGC(display, 0), 100, 100, "Thanks for Watching!", 20);
         }
